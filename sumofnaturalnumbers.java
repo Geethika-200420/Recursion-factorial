b8c7471e02864d5f6abe8d9606a15a61ec93fa62
Geethika-200420 <mallemputigeethika@gmail.com>
2025-06-04 11:18:12 +0530

    Add files via upload

---
+++ sumofnaturalnumbers.java
@@ -0,0 +1,14 @@
+public class sumofnaturalnumbers{
+    static int sumNaturalNumbers(int n){
+        if(n<=1){
+            return 1;
+        }else{
+            return n + sumNaturalNumbers(n-1);
+        }
+    }
+    public static void main(String[]args){
+        int number = 10;
+        int result = sumNaturalNumbers(number);
+        System.out.println("sum of first" +number+ "naturalnumber is"+result);
+    }
+}
